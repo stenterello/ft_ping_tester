588439a629e5a533c5f28f53fecb5deca81ba3f2
Davide Delladio <davide.delladio@cy4gate.com>
2024-09-09 18:12:53 +0200

    add: itertools for Comparer

--- src/app/comparer.rs
+++ src/app/comparer.rs
@@ -1,11 +1,17 @@
+use itertools::{
+    EitherOrBoth::{self, *},
+    Itertools,
+};
+
 pub struct Comparer;
 
 impl Comparer {
     pub fn compare_output(ft_ping_output: &mut Vec<String>, ping_output: &Vec<String>) -> bool {
-        ft_ping_output
-            .iter_mut()
-            .zip(ping_output.iter())
-            .all(|(s1, s2)| s1.replace("ft_ping", "ping").eq(s2))
+        let mut v = ft_ping_output.iter_mut().zip_longest(ping_output.iter());
+        v.all(|t: EitherOrBoth<&mut String, &String>| match t {
+            Both(s1, s2) => s1.replace("ft_ping", "ping").eq(s2),
+            _ => return false,
+        })
     }
 
     pub fn compare_exit_code(ft_ping_code: u8, ping_code: u8) -> bool {
